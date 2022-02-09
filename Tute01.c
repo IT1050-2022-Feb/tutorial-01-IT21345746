7946eab35c6bb61445c416cb5c7e37f0cb2b5088
Dananji  Paramitha <99312048+IT21345746@users.noreply.github.com>
2022-02-09 11:42:04 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,21 @@
 
 int main() {
   
+  float num1 , num2;
+  float avg;
+
+  printf("Enter the first mark:");
+  scanf("%f",&num1);
+
+   printf("Enter the second mark:");
+  scanf("%f",&num2);
+
+  avg = (num1 + num2)/2;
+
+  printf("Average is %.2f",avg);
+
+
+
   return 0;
 }
 
--- Tute04.c
+++ Tute04.c
@@ -5,6 +5,9 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+minimum(int,int);
+maximum(int,int);
+multiply(int,int);
 
 int main() {
    int no1, no2;
@@ -16,4 +19,25 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+
+minimum( no1, no2)
+{
+  if (no1<no2)
+  minimum = no1;
+  else
+  minimum = n02;
+}
+
+maximum( no1, no2)
+{
+  if (int no1>no2)
+  maximum = no1;
+  else
+  minimum = no2;
+} 
+
+multiply( no1, no2)
+{
+  multiply = no1*no2;
 }
